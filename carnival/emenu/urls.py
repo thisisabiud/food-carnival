eae98adf2faf1faed802fe4fc5e40c0b4b1d445f
thisisabiud <iam.abiud@gmail.com>
2024-10-06 18:48:09 +0300

    updates

--- carnival/emenu/urls.py
+++ carnival/emenu/urls.py
@@ -6,5 +6,4 @@ app_name = 'emenu'
 urlpatterns = [
     path('', views.menu_list, name='menu_list'),
     path('<int:id>', views.menu, name='menu_detail'),
-    path('pdf/<int:id>', views.vendor_menu_pdf, name='vendor_menu_pdf'),
 ]
